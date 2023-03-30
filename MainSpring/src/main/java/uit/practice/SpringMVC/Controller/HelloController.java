9a99a2f46340fef43f90842737767cd377b5f140
UIT.19521675 <70103187+MaiKhanh19521675@users.noreply.github.com>
2023-03-30 14:27:32 +0700

    second commit

--- MainSpring/src/main/java/uit/practice/SpringMVC/Controller/HelloController.java
+++ MainSpring/src/main/java/uit/practice/SpringMVC/Controller/HelloController.java
@@ -1,4 +1,4 @@
-package stackjava.com.springmvchello.controller;
+package uit.practice.SpringMVC.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,14 +7,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HelloController {
 
-	@RequestMapping("/")
+	@RequestMapping(value = { "/", "/trang-chu" })
 	public String index() {
-		return "index";
+		return "user/index";
 	}
 
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public String hello() {
-		return "hello";
+		return "user/hello";
 	}
 
 }
